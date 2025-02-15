187f29b02f87c895077bc7c25f4d2a889d946345
camil <camillegalicia00@gmail.com>
2025-02-16 00:25:25 +0800

    added terms & condition checkbox & hyperlink for modal in register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -110,6 +110,20 @@
                                     <a href="{{ url('/admin/home') }}" class="" style="text-decoration: underline; color: #742F2F;">Sign In</a>
                                 </div>
                             </div>
+
+                            <div class="form-group text-center top">
+                                <div class="col-md-12">
+                                    <span>
+                                    <input type="checkbox" name="agree_terms" required>  
+                                        I have read and agree to the 
+                                        <a href="#" 
+                                        data-toggle="modal"
+                                        data-target=".bd-example-modal-lg" 
+                                        data-bs-target="#termsModal"
+                                        style="text-decoration: underline; color:rgb(16, 50, 97);">Terms and Conditions</a>.
+                                    </span>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
