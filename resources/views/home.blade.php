842c07bfc65851e0d1a7e6f34ef3aa30fb0a5d3a
Acel Margallo <104260984+Acelgab@users.noreply.github.com>
2025-02-27 23:24:05 +0800

    dashboard UI polish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-10">
             <div class="panel panel-default">
-                <div class="panel-heading">@lang('quickadmin.qa_dashboard')</div>
+                <div class="panel-heading" id="dash">@lang('quickadmin.qa_dashboard')</div>
 
                 <div class="panel-body">
                     @lang('quickadmin.qa_dashboard_text')
@@ -21,7 +21,7 @@
         <div class="panel-body table-responsive">
             <div class="row">
                 <div class="col-md-6">
-                    <table class="table table-bordered table-striped">
+                    <table class="table">
                         <tr>
                             <th>@lang('quickadmin.users.fields.name')</th>
                             <td field-key='name'>{{ $user->name }}</td>
